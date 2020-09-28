fdf284a2071dfa6db6d35e118e9e7d0dc304b9be
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-28 18:57:31 +0000

    filling out the urls_index template

--- express_server.js
+++ express_server.js
@@ -4,6 +4,11 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
