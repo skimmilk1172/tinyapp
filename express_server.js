ce5fe75d7a39d16b50dea500dc992c833c930b28
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-29 22:33:30 +0000

    Updated URLs

--- express_server.js
+++ express_server.js
@@ -52,12 +52,18 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };;
+  res.render("urls_show", templateVars);
 });
+
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect('/urls');
 })
+
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL;
+  res.redirect(`/urls`);
+});
